6f7c702004d4d999dcfdcda2dc538e5e4b8c69af
Rubén Martín Lozano <ruben.martin.lozano@gmail.com>
2018-04-13 17:03:34 +0200

    fix className

--- src/markers.jsx
+++ src/markers.jsx
@@ -27,7 +27,7 @@ const Markers = ({ colors, frets, hasNut, height, leftHanded, lineDeflect, lineT
 
           components.push(
             <circle
-              className='sb-ChordMaker'
+              className='sb-ChordMarker'
               fill={colors.marker}
               key={`mark${index}`}
               cx={cx}
